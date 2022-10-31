d3bc6e2c639e9c179b7b4bfb3e8cd37d1a422ed6
david <46906261+dpedrosac@users.noreply.github.com>
2022-10-31 16:08:51 +0100

    add via upload

---
+++ dependencies.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+# ====================    General settings for GUIs/functions within ./utils   ====================
+ROOTDIR = os.path.dirname(os.path.realpath(__file__))
+FILEDIR = os.path.join(ROOTDIR, 'data')
+GITHUB = 'https://github.com/Michaela000/DBS-Patients'
