25878bbd57a3786168238c42fb1dd23b3f6c5aed
LErlebach <142877079+LErlebach@users.noreply.github.com>
2023-08-22 14:30:47 +0200

    Update dependencies.py
    Test push 2

--- dependencies.py
+++ dependencies.py
@@ -1,18 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-
-# ====================    General settings for GUIs/functions within ./utils   ====================
-ROOTDIR = os.path.dirname(os.path.realpath(__file__))
-FILEDIR = os.path.join(ROOTDIR, 'data')
-GITHUB = 'https://github.com/Michaela000/DBS-Patients'
-
-#Test: first push marco
-
-(3+5)
-print(3 + 5)
-print(4 * 20)
-print(1600 / 2)
-print(2 * (1000 + 3000))
-
+#!/usr/bin/env python# -*- coding: utf-8 -*-import os# ====================    General settings for GUIs/functions within ./utils   ====================ROOTDIR = os.path.dirname(os.path.realpath(__file__))FILEDIR = os.path.join(ROOTDIR, 'data')GITHUB = 'https://github.com/Michaela000/DBS-Patients'#Test: first push marco#Test: push 1 Layne(3+5)print(3 + 5)print(4 * 20)print(1600 / 2)print(2 * (1000 + 3000))#Test: push 2 Layneprint(90000 - 10000)print(200000 + 600000)
