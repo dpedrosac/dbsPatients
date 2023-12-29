2dfe970ef1007f0d81bd6f17e95ea9837583f852
david <david.pedrosa@staff.uni-marburg.de>
2023-12-29 01:01:31 +0100

    Added the toggle buttons to the GUI in which the DBS settings are entered. And debugging

--- GUI/GUIsettingsDBS.py
+++ GUI/GUIsettingsDBS.py
@@ -23,6 +23,7 @@ class DBSsettingsDialog(QWidget):
         self.rows = rows
         self.date = visit  # ensures the right date is entered
         self.group_layouts_contacts = []  # this ensures that group 2 may be made visible or not
+        self.group_layouts_settings = []  # this ensures that group 2 may be made visible or not
 
         self.adjustSize()
         self.setup_ui()
@@ -68,63 +69,8 @@ class DBSsettingsDialog(QWidget):
 
         return contacts, contactsContent
 
-    def DBSsettings_specific_new(self, side, obj, obj_content):
-        """generic function to fill the created optionbox with specific content according to the [LEADS] information"""
-        from dependencies import LEADS
-
-        idx = 0 if side == 'left' else 1
-        try:
-            num_contacts = int(LEADS[self.lineEditLeads.currentText()]['Contacts_per_side'])
-            contact_name = LEADS[self.lineEditLeads.currentText()]['Contacts_name'][idx]
-            contact_name.insert(0, 'IPG')
-        except KeyError:
-            return obj, obj_content
-
-
-        group_layouts = []
-
-        for group_number in range(1, 3):
-            layout = self.create_group_layout(side, contact_name, num_contacts, group_number)
-            group_layouts.append(layout)
-            obj_content.addLayout(layout)
-
-        obj_content.addStretch(2)
-        obj.hide()
-
-        # Store references to the created layouts
-        self.group_layouts_contacts.extend(group_layouts)
-
-        return obj, obj_content
-
-    def create_group_layout(self, side, contact_name, num_contacts, group_number):
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel(f"Group {group_number}"))
-
-        anode_grid = Content.create_grid_columntitle(side_label=side.capitalize(), name_title="Anode",
-                                                     num_rows=num_contacts + 1)
-        cathode_grid = Content.create_grid_columntitle(side_label=side.capitalize(), name_title="Cathode",
-                                                       num_rows=num_contacts + 1)
-
-        dbs_percentage_layout = QHBoxLayout()
-        if group_number == 1:
-            FirstColumnNames = Content.create_first_column(num_rows=num_contacts + 1,
-                                                           string2use=contact_name)
-            dbs_percentage_layout.addLayout(FirstColumnNames)
-
-        dbs_percentage_layout.addLayout(anode_grid)
-        dbs_percentage_layout.addLayout(cathode_grid)
-
-        layout.addLayout(dbs_percentage_layout)
-
-        if group_number == 2:
-            toggleButton = QPushButton('+', self)
-            toggleButton.setFixedSize(20, 20)
-            layout.addWidget(toggleButton)
-
-        return layout
-
     def DBSsettings_specific(self, side, obj, obj_content):
-        """generic function to fill the created optionbox with specific content according to the [LEADS] information"""
+        """Generic function to fill the created optionbox with specific content according to the [LEADS] information"""
         from dependencies import LEADS
 
         idx = 0 if side == 'left' else 1
@@ -135,9 +81,8 @@ class DBSsettingsDialog(QWidget):
         except KeyError:
             return obj, obj_content
 
-        group_layout = []
         for group_number in range(1, 3):
-            layout = QVBoxLayout()
+            layout, dbs_percentage_layout = QVBoxLayout(), QHBoxLayout()
             layout.addWidget(QLabel(f"Group {group_number}"))
 
             anode_grid = Content.create_grid_columntitle(side_label=side.capitalize(), name_title="Anode",
@@ -145,29 +90,33 @@ class DBSsettingsDialog(QWidget):
             cathode_grid = Content.create_grid_columntitle(side_label=side.capitalize(), name_title="Cathode",
                                                            num_rows=num_contacts + 1)
 
-            dbs_percentage_layout = QHBoxLayout()
             if group_number == 1:
                 FirstColumnNames = Content.create_first_column(num_rows=num_contacts + 1,
                                                                string2use=contact_name)
                 dbs_percentage_layout.addLayout(FirstColumnNames)
 
             dbs_percentage_layout.addLayout(anode_grid)
-            dbs_percentage_layout.addLayout(cathode_grid)
+            dbs_percentage_layout.addLayout(cathode_grid)  # adds both layouts
 
+            if group_number == 1:
+                self.set_lineedit_state(dbs_percentage_layout, enabled=True)
             layout.addLayout(dbs_percentage_layout)
-            group_layout.append(layout)
 
             if group_number == 2:
-                toggleButton = QPushButton('+', self)
-                toggleButton.setFixedSize(20, 20)  # Set a fixed size
-                obj_content.addWidget(toggleButton)
+                toggle_layout = QVBoxLayout()
+                toggleButton1 = QPushButton('+', self)
+                toggleButton1.setFixedSize(20, 20)  # Set a fixed size
+                toggleButton2 = QPushButton('-', self)
+                toggleButton2.setFixedSize(20, 20)  # Set a fixed size
+                toggle_layout.addWidget(toggleButton1)
+                toggle_layout.addWidget(toggleButton2)
+                obj_content.addLayout(toggle_layout)
             obj_content.addLayout(layout)
+            self.group_layouts_contacts.append(dbs_percentage_layout)  # needed to toggle visibility of 2nd group later
 
-            # Store references to the created layouts
-            self.group_layouts_contacts.extend(group_layout)
-
-        toggleButton.clicked.connect(self.toggle_content_visibilityPercentage)
-        # self.set_initial_content_statePercentage()
+        # Define what to do when button is pressed
+        toggleButton1.clicked.connect(self.enable_SecondGroup)
+        toggleButton2.clicked.connect(self.disable_SecondGroup)
 
         obj_content.addStretch(2)
         obj.hide()
@@ -217,13 +166,29 @@ class DBSsettingsDialog(QWidget):
         self.lineEditLeadManufacturer.currentIndexChanged.connect(self.changed_index_comboboxIPG)
         self.lineEditIPG.currentIndexChanged.connect(self.changed_index_comboboxSystem)
 
+    def lead_manufacturer(self):
+        """Creates ComboBox that might be hidden or shown depending upon the input in ComboBoxes above"""
+
+        leads_layout = QHBoxLayout()
+        self.LabelLeads = QLabel('Implanted Leads?')
+        self.lineEditLeads = QComboBox()
+        self.lineEditLeads.addItem('')
+
+        leads_layout.addWidget(self.LabelLeads)
+        leads_layout.addWidget(self.lineEditLeads)
+        leads_layout.addStretch()
+
+        self.lineEditLeads.currentIndexChanged.connect(self.changed_index_comboboxLeads)
+
+        return leads_layout
+
     def hideLeadsLayoutIfNeeded(self):
         """the leads combobox should only be called if there is some content in the system Combobox above"""
         if self.lineEditLeadManufacturer.currentText() == 'Please select a manufacturer':
             Content.object_visibility(self.leads_layout, action='hide')
 
     def DBS_parameters(self, layout_general, side, num_rows=3):
-        """Optionbox for the DBS settings """
+        """Optionbox for the DBS settings amplitude, pulse width and frequency """
 
         idx = 0 if side == 'left' else 1
         self.optionbox_dbs_settings = QGroupBox(f'DBS parameters - {side.capitalize()} target')
@@ -242,12 +207,11 @@ class DBSsettingsDialog(QWidget):
                 dbs_settings_layout.addLayout(FirstColumnNames)
 
             dbs_settings_layout.addLayout(settings_grid)
-            group_layout.addLayout(dbs_settings_layout)
+            if group_number == 1:
+                self.set_lineedit_state(dbs_settings_layout, enabled=True)
 
-            if group_number == 2:
-                toggleButton = QPushButton('+', self)
-                toggleButton.setFixedSize(20, 20)  # Set a fixed size
-                self.optionbox_dbs_settingsContent.addWidget(toggleButton)
+            group_layout.addLayout(dbs_settings_layout)
+            self.group_layouts_settings.append(dbs_settings_layout)
             self.optionbox_dbs_settingsContent.addLayout(group_layout)
 
         return self.optionbox_dbs_settings
@@ -267,26 +231,6 @@ class DBSsettingsDialog(QWidget):
 
         # Connect signals to slots
         # self.button_save_return.clicked.connect(self.onClickedSaveReturn)
-
-    def lead_manufacturer(self):
-        """Creates optionbox that might be hidden or shown when other depending upon the input in other ComboBox"""
-
-        leads_layout = QHBoxLayout()
-        self.LabelLeads = QLabel('Implanted Leads?')
-        self.lineEditLeads = QComboBox()
-        self.lineEditLeads.addItem('')
-
-        leads_layout.addWidget(self.LabelLeads)
-        leads_layout.addWidget(self.lineEditLeads)
-        leads_layout.addStretch()
-
-        leads_layout.addWidget(self.LabelLeads)
-        leads_layout.addWidget(self.lineEditLeads)
-        leads_layout.addStretch()
-
-        self.lineEditLeads.currentIndexChanged.connect(self.changed_index_comboboxLeads)
-
-        return leads_layout
 
     def adapt_combobox(self, obj, action):
         """Update items in QComboBox; different actions are available, see below"""
@@ -308,15 +252,20 @@ class DBSsettingsDialog(QWidget):
                     return combo_box.currentText()
 
     def actions_for_selected(self):
-        """defines the actions to be taken when ComboBoxes are selected"""
+        """Define actions to be taken when ComboBoxes are changed."""
 
         lead_selection = self.adapt_combobox(self.leads_layout, action='return_contents')
-        if (self.lineEditLeadManufacturer.currentText() != 'Please select a manufacturer' and
-                lead_selection != 'Please select the implanted leads' and
-                self.lineEditIPG.currentText() != 'Please select an IPG'):
+        manufacturer_selected = self.lineEditLeadManufacturer.currentText() != 'Please select a manufacturer'
+        leads_selected = lead_selection != 'Please select the implanted leads'
+        ipg_selected = self.lineEditIPG.currentText() != 'Please select an IPG'
 
-            self.settings_left, self.settings_leftContent = self.DBSsettings_specific(side='left', obj=self.settings_left, obj_content=self.settings_leftContent)
-            self.settings_right, self.settings_rightContent = self.DBSsettings_specific(side='right', obj=self.settings_right, obj_content=self.settings_rightContent)
+        if manufacturer_selected and leads_selected and ipg_selected:
+            self.settings_left, self.settings_leftContent = self.DBSsettings_specific(
+                side='left', obj=self.settings_left, obj_content=self.settings_leftContent
+            )
+            self.settings_right, self.settings_rightContent = self.DBSsettings_specific(
+                side='right', obj=self.settings_right, obj_content=self.settings_rightContent
+            )
 
             self.settings_left.show()
             self.settings_right.show()
@@ -337,21 +286,38 @@ class DBSsettingsDialog(QWidget):
                 else:
                     self.clear_layout(item.layout())
 
-    def disable_lineedits(self, layout):
+    def set_lineedit_state(self, layout, enabled=False):
         for i in range(layout.count()):
-            widget = layout.itemAt(i).widget()
-            if isinstance(widget, QLineEdit):
-                widget.setEnabled(False)
-            elif isinstance(widget, QLayout):
-                self.disable_lineedits(widget)  # Recursively handle nested layouts
+            item = layout.itemAt(i)
 
-    def set_initial_content_statePercentage(self):
-        [item.widget().setEnabled(False) for widget in [self.DBSpercentageLeft2Anode, self.DBSpercentageLeft2Cathode]
-         for i in range(widget.count()) if (item := widget.itemAt(i)) is not None and item.widget() is not None]
+            if item is not None:
+                widget = item.widget()
 
-    def toggle_content_visibilityPercentage(self):
-        self.disable_lineedits(self.group_layouts_contacts[1])
-        self.disable_lineedits(self.group_layouts_contacts[3])
+                if isinstance(widget, QLineEdit):
+                    widget.setEnabled(enabled)
+
+                elif isinstance(widget, QLayout):
+                    self.set_lineedit_state(widget, enabled)  # Recursively handle nested layouts
+
+                elif widget is None:
+                    # If the item is not a widget, it might be a layout or spacer.
+                    # Check if it's a layout and handle it recursively.
+                    if isinstance(item, QLayout):
+                        self.set_lineedit_state(item, enabled)
+
+    @QtCore.pyqtSlot()
+    def disable_SecondGroup(self):
+        self.set_lineedit_state(self.group_layouts_contacts[1], enabled=False)
+        self.set_lineedit_state(self.group_layouts_contacts[3], enabled=False)
+        self.set_lineedit_state(self.group_layouts_settings[1], enabled=False)
+        self.set_lineedit_state(self.group_layouts_settings[3], enabled=False)
+
+    @QtCore.pyqtSlot()
+    def enable_SecondGroup(self):
+        self.set_lineedit_state(self.group_layouts_contacts[1], enabled=True)
+        self.set_lineedit_state(self.group_layouts_contacts[3], enabled=True)
+        self.set_lineedit_state(self.group_layouts_settings[1], enabled=True)
+        self.set_lineedit_state(self.group_layouts_settings[3], enabled=True)
 
     @QtCore.pyqtSlot()
     def changed_index_comboboxIPG(self):
